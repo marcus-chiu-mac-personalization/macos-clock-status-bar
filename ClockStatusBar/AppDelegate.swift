dd36bfc3b4a1a0e741489cb3cb7dd8660ff630dc
Marcus Chiu <marcuschiu9@gmail.com>
2020-06-23 18:18:23 -0500

    initial commit

--- ClockStatusBar/AppDelegate.swift
+++ ClockStatusBar/AppDelegate.swift
@@ -12,28 +12,72 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
+    var statusItem: NSStatusItem!
+    var detailedDateMenuItem: NSMenuItem!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        statusItem = NSStatusBar.system.statusItem(withLength: 40)
+        let statusMenu = NSMenu(title: "Clock Status Bar Menu")
+        statusItem.menu = statusMenu
+        statusMenu.addItem(
+            withTitle: "",
+            action: nil,
+            keyEquivalent: "")
+        statusMenu.addItem(
+            withTitle: "Quit",
+            action: #selector(AppDelegate.ExitNow(sender:)),
+            keyEquivalent: "")
+        detailedDateMenuItem = statusMenu.item(at: 0)
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        updateDetailedDate()
+        updateClock()
+        
+        scheduledTimerWithTimeInterval()
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func scheduledTimerWithTimeInterval() {
+        // Scheduling timer to Call the function "updateClock" with the interval of 1 seconds
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateClock), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 3600, target: self, selector: #selector(self.updateDetailedDate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateDetailedDate() {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM dd, yyyy"
+        detailedDateMenuItem?.title =  dateFormatter.string(from: date)
     }
 
-
+    @objc func updateClock() {
+        let _date = Date()
+        var hour = Calendar.current.component(.hour, from: _date) % 12
+        let minute = Calendar.current.component(.minute, from: _date)
+        if (hour == 0) {
+            hour = 12
+        }
+        let hourHexString = convertHourToHex(hour: hour)
+        let minuteString = String(format: "%02d", minute)
+        statusItem?.button?.title = "\(hourHexString):\(minuteString)"
+    }
+    
+    func convertHourToHex(hour: Int) -> String {
+        var hourHex = ""
+        switch hour {
+        case 10:
+            hourHex = "A"
+        case 11:
+            hourHex = "B"
+        case 12:
+            hourHex = "C"
+        default:
+            hourHex = String(hour)
+        }
+        return hourHex
+    }
+    
+    @objc func ExitNow(sender: AnyObject) {
+        NSApplication.shared.terminate(self)
+    }
 }
-
