2088797f1655813d02925162ec2175e3789f8283
Marcus Chiu <marcuschiu9@gmail.com>
2020-06-23 15:03:00 -0500

    Initial Commit

---
+++ ClockStatusBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ClockStatusBar
+//
+//  Created by Marcus Chiu on 6/23/20.
+//  Copyright © 2020 Marcus Chiu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
